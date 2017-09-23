858a4c1197b2ee3356fa29f426396b6a00017ef0
Rifka Nur Fadhilah <rifka.nur@ui.ac.id>
2017-09-23 19:03:40 +0700

    add tutorial 3

---
+++ tutorial3apap/src/main/java/com/example/tutorial3/Tutorial3apapApplication.java
@@ -0,0 +1,12 @@
+package com.example.tutorial3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial3apapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial3apapApplication.class, args);
+	}
+}
---
+++ tutorial3apap/src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,35 @@
+package com.example.tutorial3.service;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.example.tutorial3.model.StudentModel;
+
+public class InMemoryStudentService implements StudentService {
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+
+	@Override
+	public StudentModel selectStudent(String npm) {
+		for(int ii = 0; ii < studentList.size(); ii++) {
+			if(studentList.get(ii).getNpm().equals(npm)) {
+				return studentList.get(ii);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		return studentList;
+	}
+
+	@Override
+	public void addStudent(StudentModel student) {
+		studentList.add(student);
+	}
+
+	@Override
+	public void removeStudent(StudentModel student) {
+		studentList.remove(student);	
+	}
+
+}
